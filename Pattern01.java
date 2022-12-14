da57503a9547a696ccacc28a3740c64db0f940c5
Vicky5453 <113967961+Vicky5453@users.noreply.github.com>
2022-12-14 22:19:36 +0530

    Add files via upload

---
+++ Pattern01.java
@@ -0,0 +1,20 @@
+package pattern;
+
+public class Pattern01 {
+
+	public static void main(String[] args) {
+	         
+		
+		for(int row=1;row<=3;row++)
+		{
+			
+			for(int col=1;col<=row;col++)
+			{
+				System.out.print(col);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
